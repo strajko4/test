fbb68221d692ffa4abb0526d105de4f9ac506a35
strajko4 <sasa.trajkovic@volvocars.com>
2021-12-21 08:47:13 +0100

    commit

---
+++ scripts/pipeline/release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+folder=$1
+s3_bucket=$2
+s3_secret_access_key=$3
+s3_access_key=$4
+s3_region=$5
+
+# Install required dependencies for Python script
+pip3 install boto3
+
+# Run upload script
+
+python3 scripts/pipline/upload_model_folder_to_s3.py $folder $s3_bucket $s3_secret_access_key $s3_access_key $s3_region
---
+++ scripts/pipeline/upload_model_folder_to_s3.py
@@ -0,0 +1,48 @@
+import os
+import sys 
+#sys.path.append('..')
+#from AWSS3.aws_bucket_client import bucket_client
+#from config import Config
+import boto3
+
+
+def main():
+    if (len(sys.argv)!=5):
+        print('Error: Required 5 arguments.')
+        sys.exit(1)
+    
+    folder = sys.argv[1]
+    s3_bucket = sys.argv[2]
+    s3_secret_access_key = sys.argv[3]
+    s3_access_key = sys.argv[4]
+    s3_region = sys.argv[5]
+
+    session = boto3.session(service_name = "s3",
+        region_name = s3_region,
+        aws_access_key_id = s3_access_key,
+        aws_secret_access_key = s3_secret_access_key,
+    )
+
+    s3_client = session.client()
+
+    root = "..\\" + folder
+    for path, subdirs, files in os.walk(root):
+        if 'pycache' in path.split('\\')[-1]:
+            continue
+        for file in files:
+            full_path = os.path.join(path, file)
+            dest_path = full_path.split('..\\')[-1].replace('\\', '/')
+            try:
+                print('Uploading file: ' + dest_path)
+                s3_client.upload_file(full_path,
+                                    s3_bucket,
+                                    dest_path,
+                                    )
+                print('Upload succesfull!')
+
+            except Exception as e:
+                # This is a catch all exception
+                print("Error happened when uploading the file: ", e)
+                return e
+
+main()
